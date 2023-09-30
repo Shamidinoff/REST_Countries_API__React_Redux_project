537dcbeef022b975ecc4c600f509fad3efa792d8
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-09-30 21:22:42 +0600

    REST Countries API - React Redux project

---
+++ src/store/details/details-actions.js
@@ -0,0 +1,30 @@
+//import { setLoading } from "../countries/countries-actions";
+
+export const SET_LOADING = "@@details/SET_LOADING";
+export const SET_ERROR = "@@details/SET_ERROR";
+export const SET_COUNTRY = "@@details/SET_COUNTRY";
+
+const setLoading = () => ({
+  type: SET_LOADING,
+});
+
+const setError = (err) => ({
+  type: SET_ERROR,
+  payload: err,
+});
+
+const setCountry = (country) => ({
+  type: SET_COUNTRY,
+  payload: country,
+});
+
+export const loadCountryByName =
+  (name) =>
+  (dispatch, _, { client, api }) => {
+    dispatch(setLoading());
+
+    client
+      .get(api.searchByCountry(name))
+      .then(({ data }) => dispatch(setCountry(data)))
+      .catch((err) => dispatch(setError(err.message)));
+  };
